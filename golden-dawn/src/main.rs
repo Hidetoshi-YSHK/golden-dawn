43a724971271c0a6b01c1def4c5efee3abf23a13
Hidetoshi <mayoiga777@gmail.com>
2021-11-21 22:59:22 +0900

    Add chrono

--- golden-dawn/src/main.rs
+++ golden-dawn/src/main.rs
@@ -1,7 +1,11 @@
-#![windows_subsystem = "windows"]
+//#![windows_subsystem = "windows"]
 
 use std::fs;
 
+use chrono::prelude::*;
+
 fn main() {
-    fs::create_dir("20211121").ok();
+    let today = Local::today();
+    let today_dir_name = today.format("%Y-%m-%d").to_string();
+    fs::create_dir(today_dir_name).ok();
 }
