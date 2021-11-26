a5898cbb70fbf0aed0cf4c02459c4240644b1848
kawakubo <kawakubo.hidetoshi@meta.co.jp>
2021-11-26 17:00:25 +0900

    Add build script and resource

--- golden-dawn/src/main.rs
+++ golden-dawn/src/main.rs
@@ -5,6 +5,14 @@ use std::fs;
 use std::path::PathBuf;
 use std::env::current_exe;
 use chrono::prelude::*;
+use serde_derive::Deserialize;
+
+#[derive(Deserialize)]
+struct Config {
+    tmp_dir: String,
+    days_to_move: u32,
+    days_to_remove: u32,
+}
 
 fn main() {
     let exe_dir = get_exe_dir().expect("Could not get exe directory.");
@@ -12,7 +20,10 @@ fn main() {
     create_today_dir(&exe_dir, "%Y-%m-%d")
         .expect("Could not create today's directory.");
 
-    move_old_dir();
+    let dirs = get_dirs(&exe_dir).expect("Cound not get directories.");
+    for dir in dirs {
+        println!("{}", dir.display());
+    }
 }
 
 fn get_exe_dir() -> io::Result<PathBuf> {
@@ -26,9 +37,25 @@ fn create_today_dir(parent_dir: &PathBuf, format: &str) -> io::Result<()> {
     let today_dir_name = today.format(format).to_string();
     let mut path_buf = parent_dir.clone();
     path_buf.push(&today_dir_name);
-    fs::create_dir(path_buf)
+    if path_buf.exists() {
+        Ok(())
+    } else {
+        fs::create_dir(path_buf)
+    }
 }
 
+fn get_dirs(parent_dir: &PathBuf) -> io::Result<Vec<PathBuf>> {
+    let read_dir = fs::read_dir(parent_dir)?;
+    let mut dirs = Vec::new();
+    for dir_entry in read_dir {
+        let dir_entry = dir_entry?;
+        let metadata = dir_entry.metadata()?;
+        if metadata.is_dir() {
+            dirs.push(dir_entry.path());
+        }
+    }
+    Ok(dirs)
+}
 
 
 fn move_old_dir() {
