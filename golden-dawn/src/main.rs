6762ab6bbab1474cad040002f00b2f9dc143f79a
kawakubo <kawakubo.hidetoshi@meta.co.jp>
2021-11-26 18:08:33 +0900

    WIP: develop

--- golden-dawn/src/main.rs
+++ golden-dawn/src/main.rs
@@ -2,40 +2,51 @@
 
 use std::io;
 use std::fs;
-use std::path::PathBuf;
+use std::fs::File;
+use std::path::{Path, PathBuf};
 use std::env::current_exe;
 use chrono::prelude::*;
+use serde::__private::ser::constrain;
 use serde_derive::Deserialize;
+
+const CONFIG_FILE: &str = "config.toml";
 
 #[derive(Deserialize)]
 struct Config {
     tmp_dir: String,
+    date_format: String,
     days_to_move: u32,
     days_to_remove: u32,
 }
 
 fn main() {
-    let exe_dir = get_exe_dir().expect("Could not get exe directory.");
+    let config_str = read_config_file().expect("Could not read config file.");
+    let config = toml::from_str::<Config>(&config_str)
+        .expect("Could not parse config file.");
 
-    create_today_dir(&exe_dir, "%Y-%m-%d")
+    let tmp_dir = Path::new(&config.tmp_dir);
+
+    create_today_dir(tmp_dir, "%Y-%m-%d")
         .expect("Could not create today's directory.");
 
-    let dirs = get_dirs(&exe_dir).expect("Cound not get directories.");
+    let dirs = get_dirs(tmp_dir).expect("Cound not get directories.");
     for dir in dirs {
         println!("{}", dir.display());
     }
 }
 
-fn get_exe_dir() -> io::Result<PathBuf> {
-    let mut path_buf = current_exe()?;
-    path_buf.pop();
-    Ok(path_buf)
+fn read_config_file() -> io::Result<String> {
+    let mut exe_dir = current_exe()?;
+    exe_dir.pop();
+
+    let config_file = Path::join(&exe_dir, CONFIG_FILE);
+    Ok(fs::read_to_string(config_file)?)
 }
 
-fn create_today_dir(parent_dir: &PathBuf, format: &str) -> io::Result<()> {
+fn create_today_dir(parent_dir_path: &Path, format: &str) -> io::Result<()> {
     let today = Local::today();
     let today_dir_name = today.format(format).to_string();
-    let mut path_buf = parent_dir.clone();
+    let mut path_buf = parent_dir_path.to_path_buf();
     path_buf.push(&today_dir_name);
     if path_buf.exists() {
         Ok(())
@@ -44,7 +55,7 @@ fn create_today_dir(parent_dir: &PathBuf, format: &str) -> io::Result<()> {
     }
 }
 
-fn get_dirs(parent_dir: &PathBuf) -> io::Result<Vec<PathBuf>> {
+fn get_dirs(parent_dir: &Path) -> io::Result<Vec<PathBuf>> {
     let read_dir = fs::read_dir(parent_dir)?;
     let mut dirs = Vec::new();
     for dir_entry in read_dir {
@@ -56,7 +67,6 @@ fn get_dirs(parent_dir: &PathBuf) -> io::Result<Vec<PathBuf>> {
     }
     Ok(dirs)
 }
-
 
 fn move_old_dir() {
     let read_dir = fs::read_dir(".").unwrap();
